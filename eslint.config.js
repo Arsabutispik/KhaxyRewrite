afb2d09c5224023cbf703df158cf5f949d0d5f6b
ispik <ispik6441@proton.me>
2025-02-27 11:22:46 +0300

    workflow testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import i18nJson from "eslint-plugin-i18n-json";
 
 export default [
-    { files: ["**/*.{js,mjs,cjs,ts}"] },
-    { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["locales/**/*.json"],
+    plugins: {
+      "i18n-json": i18nJson,
+    },
+    rules: {
+      "i18n-json/identical-keys": ["error", { filePath: "locales/en.json" }],
+      "i18n-json/valid-message-syntax": "error",
+    },
+  },
 ];
